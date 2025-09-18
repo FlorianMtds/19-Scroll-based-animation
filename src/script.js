c06268da00f7731be83f61fa4ec0dae3d4823f2d
Florian <florianmtddev@gmail.com>
2025-09-18 21:56:08 +0200

    Mesh position and animation

--- src/script.js
+++ src/script.js
@@ -45,6 +45,7 @@ const material = new THREE.MeshToonMaterial({
 /**
  * Meshes
  */
+const objectsDistance = 4
 const mesh1 = new THREE.Mesh(
     new THREE.TorusKnotGeometry(1, 0.4, 16, 60),
     material
@@ -60,7 +61,17 @@ const mesh3 = new THREE.Mesh(
     material
 )
 
+mesh1.position.y = - objectsDistance * 0
+mesh2.position.y = - objectsDistance * 1
+mesh3.position.y = - objectsDistance * 2
+
+mesh1.position.x = 2
+mesh2.position.x = - 2
+mesh3.position.x = 2
+
 scene.add(mesh1, mesh2, mesh3)
+
+const sectionMeshes = [ mesh1, mesh2, mesh3 ]
 
 /**
  * Lights
@@ -111,6 +122,17 @@ renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 /**
+ * Scroll
+ */
+let scrollY = window.scrollY
+
+window.addEventListener('scroll', () =>
+{
+    scrollY = window.scrollY
+})
+
+
+/**
  * Animate
  */
 const clock = new THREE.Clock()
@@ -118,6 +140,16 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    // Animate camera
+    camera.position.y = - scrollY / sizes.height * objectsDistance
+
+    // Animate meshes
+    for(const mesh of sectionMeshes)
+    {
+        mesh.rotation.x = elapsedTime * 0.1
+        mesh.rotation.y = elapsedTime * 0.12
+    }
 
     // Render
     renderer.render(scene, camera)
